f03d2d8583f2a8292e4fc3547cceff5e3bc003ba
jake-hh <jakob.holzer.m@gmail.com>
2024-10-25 18:41:35 +0200

    Implement `save` feature

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use clap::{arg, command, value_parser, Command};
 use color_eyre::eyre;
 use std::path::Path;
+use std::io::Write;
 use std::fs;
 
 static PATH_FILE: &str = "/tmp/myfile";
@@ -28,6 +29,10 @@ fn main() -> eyre::Result<()> {
 			Command::new("save")
 				.about("Save path")
                 .arg(
+					arg!([path] "Current directory path")
+					.required(true)
+					.value_parser(value_parser!(String)))
+                .arg(
 					arg!([num] "Optional path number")
 					.value_parser(value_parser!(u8).range(1..10))))
         .get_matches();
@@ -35,7 +40,7 @@ fn main() -> eyre::Result<()> {
     match args.subcommand() {
         Some(("list", _))          => list()?,
         Some(("push", _))          => push(),
-        Some(("save", subargs))    => save(subargs.get_one::<u8>("num")),
+        Some(("save", subargs))    => save(subargs.get_one::<String>("path"), subargs.get_one::<u8>("num"))?,
         Some(("restore", subargs)) => restore(subargs.get_one::<u8>("num"))?,
         _                          => restore(None)?,
 	}
@@ -57,8 +62,27 @@ fn push() {
 	println!("'wd push' was used")
 }
 
-fn save (num: Option<&u8>) {
-	println!("'wd save' was used, num is: {:?}", num);
+fn save (path: Option<&String>, num: Option<&u8>) -> eyre::Result<()> {
+
+	let id: usize = match num {
+		Some(npos) => (npos -1) as usize,
+		None => 0
+	};
+	let path_str = path.expect("current path was not provided");
+
+	println!("'wd save' was used, id is: {:?}, path is {:?}", id, path_str);
+
+	let mut lines = read_lines()?;
+
+	if id > lines.len() {
+		return Err(eyre::eyre!("invalid value '{}' for '[num]': only {} paths are saved", id + 1, lines.len()));
+	}
+	
+	lines.insert(id, path_str.clone());
+	
+	save_lines(lines)?;
+	
+	Ok(())
 }
 
 fn restore (num: Option<&u8>) -> eyre::Result<()> {
@@ -96,4 +120,21 @@ fn read_lines() -> eyre::Result<Vec<String>> {
 		result.push(line.to_string());
 	}
 	Ok(result)
+}
+
+fn save_lines (lines: Vec<String>) -> eyre::Result<()> {
+	let file = Path::new(PATH_FILE);
+
+	if !file.try_exists().expect("Can't check existence of path file") {
+		return Err(eyre::eyre!("path file doesn't exist"));
+	}
+
+	let mut buffer= fs::File::create(file)?;
+	
+	for l in lines {
+		buffer.write(l.as_bytes())?;
+		buffer.write("\n".as_bytes())?;
+	}
+
+	Ok(())
 }
