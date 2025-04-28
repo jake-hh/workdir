60071c34aca235f8d7038d510470626b17817adc
jake-hh <jakob.holzer.m@gmail.com>
2025-04-28 17:43:43 +0200

    Add `list size` arg & `l` subcommand

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,13 @@ fn main() -> eyre::Result<()> {
 
 		.subcommand(Command::new("list")
 				.about("List recent paths")
-				.aliases(["l", "ls"]))
+				.aliases(["ls"])
+				.arg(
+					arg!([size] "Optional max list size")
+					.value_parser(value_parser!(u8).range(1..LIMIT as i64))))
+
+		.subcommand(Command::new("l")
+				.about("List 5 recent paths"))
 
 		.subcommand(Command::new("restore")
 				.about("Switch to selected path")
@@ -63,7 +69,8 @@ fn main() -> eyre::Result<()> {
 		.get_matches();
 
 	match args.subcommand() {
-		Some(("list", _))          => list()?,
+		Some(("list", subargs))    => list(subargs.get_one::<u8>("size"))?,
+		Some(("l", _))                          => l()?,
 		Some(("save", subargs))    => save(subargs.get_one::<String>("path"), subargs.get_one::<u8>("pos"))?,
 		Some(("restore", subargs)) => restore(subargs.get_one::<u8>("pos"), subargs.get_flag("verbose"))?,
 		Some(("delete", subargs))  => delete(subargs.get_one::<u8>("pos"))?,
@@ -74,13 +81,31 @@ fn main() -> eyre::Result<()> {
 	Ok(())
 }
 
-fn list() -> eyre::Result<()> {
+fn list(size: Option<&u8>) -> eyre::Result<()> {
 	let lines = read_lines()?;
 
-	for i in 0..lines.len() {
-		let missing_star = if !Path::new(&lines[i]).is_dir() {" [*]"} else {""};
-		println!("[{}] {}{}", i +1, lines[i], missing_star);
-	}
+	let n: usize = match size {
+		Some(num) => {
+			if num.clone() as usize > lines.len() {
+				return Err(eyre::eyre!("invalid value '{}' for '[size]': only {} paths are saved", num, lines.len()));
+			}
+			num.clone() as usize
+		},
+		None => lines.len()
+	};
+
+	list_lines(lines, n)?;
+
+	Ok(())
+}
+
+fn l() -> eyre::Result<()> {
+	let lines = read_lines()?;
+
+	let n: usize = if lines.len() < 5 { lines.len() } else { 5 };
+
+	list_lines(lines, n)?;
+
 	Ok(())
 }
 
@@ -132,6 +157,7 @@ fn save (path: Option<&String>, pos: Option<&u8>) -> eyre::Result<()> {
 		Some(rm_id) => println!("moved: [{}] -> [{}] {}", rm_id +1, id +1, path_str),
 		None => println!("saved: [{}] {}", id +1, path_str),
 	}
+
 	Ok(())
 }
 
@@ -181,15 +207,26 @@ fn delete (pos: Option<&u8>) -> eyre::Result<()> {
 	save_lines(lines)?;
 
 	println!("deleted: [{}] {}", id +1, path_str);
+
 	Ok(())
 }
 
 fn dump_wrapper (shell: Option<&String>) -> eyre::Result<()> {
 	let function = include_str!("wrapper.sh");
 	println!("{}", function);
+
 	Ok(())
 }
 
+
+fn list_lines(lines: Vec<String>, n: usize) -> eyre::Result<()> {
+	for i in 0..n {
+		let missing_star = if !Path::new(&lines[i]).is_dir() {" [*]"} else {""};
+		println!("[{}] {}{}", i +1, lines[i], missing_star);
+	}
+
+	Ok(())
+}
 
 fn read_lines() -> eyre::Result<Vec<String>> {
 	let mut result = Vec::new();
@@ -207,6 +244,7 @@ fn read_lines() -> eyre::Result<Vec<String>> {
 			result.push(line.to_string());
 		}
 	}
+
 	Ok(result)
 }
 
