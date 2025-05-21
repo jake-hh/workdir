fa14072cacb81ea2176fe3cfb1461d384c195ec9
jake-hh <jakob.holzer.m@gmail.com>
2025-05-21 17:43:40 +0200

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![allow(dead_code, unused_variables)]
 
 use clap::{arg, command, value_parser, Arg, Command};
-use color_eyre::eyre;	// note: does not preserve printing order
+use color_eyre::eyre;
 use colored::{Colorize, ColoredString, control};
 use std::path::Path;
 use std::io::Write;
 use std::fs;
 
+// File used to store a list of paths (working directories)
 static PATH_FILE: &str = "~/.local/state/workdir";
 
 // Max amount of stored paths
 const LIMIT: usize = 20;
 
+
 fn main() -> eyre::Result<()> {
-	control::set_override(true);	// Forces colored output
+	// Force colored output regardless of TTY
+	control::set_override(true);
+	// Enable colored error messages (Does not preserve printing order)
 	color_eyre::install()?;
 
+	// Parse specified CLI args
 	let args = command!() 	// requires 'cargo' feature
 		.about("workdir - fast working directory path switcher")
 		.author("jake")
@@ -70,6 +75,7 @@ fn main() -> eyre::Result<()> {
 
 		.get_matches();
 
+	// Match CLI args
 	match args.subcommand() {
 		Some(("list", subargs))    => list(subargs.get_one::<u8>("size"))?,
 		Some(("l", _))                          => l()?,
@@ -83,16 +89,22 @@ fn main() -> eyre::Result<()> {
 	Ok(())
 }
 
+
+// List paths
 fn list(size: Option<&u8>) -> eyre::Result<()> {
 	let lines = read_lines()?;
 
+	// Get number of printed lines...
 	let n: usize = match size {
+		// ...from specified size arg
 		Some(num) => {
 			if num.clone() as usize > lines.len() {
+				// Size arg is bigger than lines => Show error msg
 				return Err(eyre::eyre!("invalid value '{}' for '[size]': only {} paths are saved", num, lines.len()));
 			}
 			num.clone() as usize
 		},
+		// ...list all lines as default
 		None => lines.len()
 	};
 
@@ -101,9 +113,11 @@ fn list(size: Option<&u8>) -> eyre::Result<()> {
 	Ok(())
 }
 
+// Short list
 fn l() -> eyre::Result<()> {
 	let lines = read_lines()?;
 
+	// Get number of printed lines
 	let n: usize = if lines.len() < 5 { lines.len() } else { 5 };
 
 	list_lines(lines, n)?;
@@ -111,50 +125,64 @@ fn l() -> eyre::Result<()> {
 	Ok(())
 }
 
+// Save or move current PWD path in path list
 fn save (path: Option<&String>, pos: Option<&u8>) -> eyre::Result<()> {
 
+	// Get new path id...
 	let id: usize = match pos {
-		Some(num) => (num -1) as usize,
-		None => 0
+		Some(num) => (num -1) as usize,  // ...from pos arg
+		None => 0                            //  ...default is 0
 	};
 
-	let path_str = path.expect("current path was not provided");
+	// Unwrap path
+	let path_str = path.expect("current path was not provided. Please use the included wrapper");
 
+	// Check if path is a directory
 	if !Path::new(path_str).is_dir() {
 		return Err(eyre::eyre!("'{}' is not a directory", path_str));
 	}
 
-	let mut lines = read_lines()?;
+	// Optional id of a line to remove
 	let mut remove: Option<usize> = None;
+	let mut lines = read_lines()?;
 	let size = lines.len();
 
+	// Check lines list
 	for i in 0..size {
 		if &lines[i] == path_str {
 			if i == id {
+				// Line already exists at that position => Show error msg
 				return Err(eyre::eyre!("path '{}' already exists on thath position", lines[i]));
 			}
+			// Line already exists at a different position => Mark old id for removal
 			remove = Some(i);
 		}
 	}
 
 	if let Some(rm_id) = remove {
+		// Check id
 		if id >= size {
 			return Err(eyre::eyre!("invalid value '{}' for '[pos]': only {} paths are saved", id + 1, size));
 		}
+		// Remove old path from list
 		lines.remove(rm_id);
 	}
 	else {
+		// Check id
 		if id > size {
 			return Err(eyre::eyre!("invalid value '{}' for '[pos]': only {} paths are saved", id + 1, size));
 		}
+		// Check size
 		else if size >= LIMIT {
 			return Err(eyre::eyre!("limit of {} saved paths reached", LIMIT))
 		}
 	}
 
+	// Save new path in list
 	lines.insert(id, path_str.clone());
 	save_lines(lines)?;
 
+	// Print confirmation message
 	match remove {
 		Some(rm_id) => print_ok("moved".cyan(), format!("{} -> {} {}", fmt_number(rm_id), fmt_number(id), path_str)),
 		None => print_ok("saved".green(), format!("{} {}", fmt_number(id), path_str)),
@@ -163,56 +191,68 @@ fn save (path: Option<&String>, pos: Option<&u8>) -> eyre::Result<()> {
 	Ok(())
 }
 
+// Change directory to one of saved paths
 fn restore (pos: Option<&u8>, verbose: bool) -> eyre::Result<()> {
 
+	// Get path id...
 	let id: usize = match pos {
-		Some(num) => (num -1) as usize,
-		None => 0
+		Some(num) => (num -1) as usize,  // ...from pos arg
+		None => 0                            //  ...default is 0
 	};
 
 	let lines = read_lines()?;
 
+	// Check id
 	if id >= lines.len() {
 		return Err(eyre::eyre!("invalid value '{}' for '[pos]': only {} paths are saved", id + 1, lines.len()));
 	}
 
+	// Get selected line
 	let line = &lines[id];
 
+	// Check if path is a directory
 	if !Path::new(line).is_dir() {
-		// remove that path from the list
+		// TODO: Remove that path from the list
 		return Err(eyre::eyre!("'{}' is not an existing directory", line));
 	}
 
-	// cd to that path
+	// Tell the wrapper to cd to selected path
 	if verbose {
-		println!("CHDIRV {}", line);
+		println!("CHDIRV {}", line);  // Use 'change dir verbose' flag
 	} else {
-		println!("CHDIR {}", line);
+		println!("CHDIR {}", line);   // Use 'change dir' flag
 	}
 
 	Ok(())
 }
 
+// Delete one of saved paths
 fn delete (pos: Option<&u8>) -> eyre::Result<()> {
 
+	// Unwrap pos
 	let id = *pos.expect("'[pos]' was not provided") as usize -1;
 
 	let mut lines = read_lines()?;
 
+	// Check id
 	if id >= lines.len() {
 		return Err(eyre::eyre!("invalid value '{}' for '[pos]': only {} paths are saved", id + 1, lines.len()));
 	}
 
+	// Get copy of path
 	let path_str: String = lines[id].clone();
 
+	// Remove path from list
 	lines.remove(id);
 	save_lines(lines)?;
 
+	// Print confirmation message
 	print_ok("deleted".purple(), format!("{} {}", fmt_number(id), path_str));
 
 	Ok(())
 }
 
+// Print shell wrapper function
 fn dump_wrapper (shell: Option<&String>) -> eyre::Result<()> {
 	let function = include_str!("wrapper.sh");
 	println!("{}", function);
@@ -221,48 +261,66 @@ fn dump_wrapper (shell: Option<&String>) -> eyre::Result<()> {
 }
 
 
+// Print n lines from path list
 fn list_lines(lines: Vec<String>, n: usize) -> eyre::Result<()> {
+
 	for i in 0..n {
-		if !Path::new(&lines[i]).is_dir() {
-			println!("{}", format!("{} {} [*]", fmt_number(i), lines[i]).strikethrough().dimmed());
-		} else {
+		// Format each line depending on whether it is a directory
+		if Path::new(&lines[i]).is_dir() {
 			println!("{} {}", fmt_number(i), lines[i]);
+		} else {
+			println!("{}", format!("{} {} [*]", fmt_number(i), lines[i]).strikethrough().dimmed());
 		};
 	}
 
 	Ok(())
 }
 
+// Get list of paths from PATH_FILE
 fn read_lines() -> eyre::Result<Vec<String>> {
 	let mut result = Vec::new();
+
+	// Expanded path file String
 	let fstr = shellexpand::tilde(PATH_FILE).into_owned();
+	// Expanded path file Path
 	let file = Path::new(&fstr);
 
+	// Check path file existence
 	if !file.try_exists().expect("can't check existence of path file") {
 		return Err(eyre::eyre!("path file doesn't exist"));
 	}
 
+	// Read path file
 	let text = fs::read_to_string(file).expect("can't read path file");
 
+	// Parse text to lines
 	for line in text.lines() {
 		if !line.is_empty() {
 			result.push(line.to_string());
 		}
 	}
 
+	// Return lines
 	Ok(result)
 }
 
+// Save list of paths to PATH_FILE
 fn save_lines (lines: Vec<String>) -> eyre::Result<()> {
+
+	// Expanded path file String
 	let fstr = shellexpand::tilde(PATH_FILE).into_owned();
+	// Expanded path file Path
 	let file = Path::new(&fstr);
 
+	// Check path file existence
 	if !file.try_exists().expect("can't check existence of path file") {
 		return Err(eyre::eyre!("path file doesn't exist"));
 	}
 
+	// Open path file
 	let mut buffer= fs::File::create(file)?;
 
+	// Write lines to file
 	for l in lines {
 		buffer.write(l.as_bytes())?;
 		buffer.write("\n".as_bytes())?;
@@ -271,14 +329,17 @@ fn save_lines (lines: Vec<String>) -> eyre::Result<()> {
 	Ok(())
 }
 
+// Print confirmation message
 fn print_ok(header: ColoredString, body: String) {
 	println!("{} {}", header.bold(), body);
 }
 
+// Get formated path id
 fn fmt_number(id: usize) -> String {
 	format!("[{}]", id +1)
 }
 
+// // Get formated path
 // fn fmt_path(id: usize, path: &String) -> String {
 // 	format!("{} {}", fmt_number(id), path)
 // }
--- src/wrapper.sh
+++ src/wrapper.sh
@@ -4,8 +4,10 @@
 # $> workdir wrapper >> ~/.zsh/custom/workdir.sh
 
 function wd {
+	# check arguments & run workdir
 	if [ -z "$1" ]
 	then
+		# when no args found
 		x=`workdir`
 	else
 		subcmd="$1"
@@ -13,20 +15,26 @@ function wd {
 
 		if [ "$subcmd" = "save" ] || [ "$subcmd" = "s" ]
 		then
+			# when save subcommand found
 			x=`workdir "$subcmd" "$PWD" "$@"`
 		else
+			# when other args found
 			x=`workdir "$subcmd" "$@"`
 		fi
 	fi
 
+	# check workdir output
 	if [ "${x::6}" = "CHDIRV" ]
 	then
+		# when output contains 'change dir verbose' flag
 		echo "changing dir to ${x:7}"
 		cd "${x:7}"
 	elif [ "${x::5}" = "CHDIR" ]
 	then
+		# when output contains 'change dir' flag
 		cd "${x:6}"
 	else
+		# when output contains no flags
 		echo "$x"
 	fi
 }
